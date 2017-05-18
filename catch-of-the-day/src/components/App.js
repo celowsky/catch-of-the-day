093f331779ad460862126bb88f587059d4ff8a7a
Christian Elowsky <christian@hardindd.com>
2017-05-18 16:52:53 -0500

    Completes lesson 11

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header cool={true} age="5000" tagline="Fishy Fish" />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+}
+
+export default App;
